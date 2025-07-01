6666bf0bca4c88dfdc7bbae037cd87db96bb1142
Ludvig <ludvig@clin.au.dk>
2025-07-01 15:46:31 +0200

    Removes remnants of htslib

--- src/cli/io.rs
+++ src/cli/io.rs
@@ -1,21 +1,8 @@
 use anyhow::{anyhow, Context, Result};
 
-use rust_htslib::bam::{IndexedReader, Read};
 use std::path::Path;
 use twobit::TwoBitFile;
 // BAM
-
-pub fn create_chromosome_reader(bam_path: &Path, chr: &str) -> Result<(IndexedReader, u32, u64)> {
-    let reader = IndexedReader::from_path(bam_path).context(format!("opening BAM for {}", chr))?;
-    let header = reader.header().to_owned();
-    let tid = header
-        .tid(chr.as_bytes())
-        .ok_or_else(|| anyhow!("{} not in BAM", chr))?;
-    let chrom_len = header
-        .target_len(tid)
-        .ok_or_else(|| anyhow!("No length for {}", chr))? as u64;
-    Ok((reader, tid, chrom_len))
-}
 
 // Reference 2bit file
 
--- src/reference/mod.rs
+++ src/reference/mod.rs
@@ -1,7 +1,6 @@
-pub mod read;
-pub mod kmer_codec;
-pub mod write;
-pub mod process_counts;
 pub mod bed;
 pub mod blacklist;
 pub mod gc;
+pub mod kmer_codec;
+pub mod process_counts;
+pub mod write;
